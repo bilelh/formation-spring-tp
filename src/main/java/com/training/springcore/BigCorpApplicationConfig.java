f72ffa498e06d8e2099ec830d33d0d6719158047
bilel <bilelh532@gmail.com>
2019-06-06 14:04:36 +0200

    TP7 profil ==> Environment OK

--- src/main/java/com/training/springcore/BigCorpApplicationConfig.java
+++ src/main/java/com/training/springcore/BigCorpApplicationConfig.java
@@ -2,11 +2,12 @@ package com.training.springcore;
 
 
 import com.training.springcore.model.ApplicationInfo;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 
 import java.util.Set;
 
@@ -14,19 +15,21 @@ import java.util.Set;
 @Configuration
 @PropertySource("classpath:application.properties")
 public class BigCorpApplicationConfig {
-    ApplicationInfo app;
 
-    @Value("${bigcorp.name}")
+    @Autowired
+    private Environment environment;
+    /*ApplicationInfo app;
     String name;
-    @Value("${bigcorp.version}")
     Integer version;
-    @Value("${bigcorp.emails}")
     Set<String> emails;
-    @Value("${bigcorp.webSiteUrl}")
-    String webSiteUrl;
+    String webSiteUrl;*/
 
     @Bean
     public ApplicationInfo applicationInfo() {
+        String name = environment.getRequiredProperty("bigcorp.name");
+        Integer version = environment.getRequiredProperty("bigcorp.version", Integer.class);
+        Set<String> emails = environment.getRequiredProperty("bigcorp.emails", Set.class);
+        String webSiteUrl = environment.getRequiredProperty("bigcorp.webSiteUrl");
         return new ApplicationInfo(name, version, emails, webSiteUrl);
     }
 
